5cb48f9310f2dfd7475621d35d9b2e5203f454a3
Thunay Moreira de Soares <thunay@buildbox.com.br>
2022-09-25 15:29:23 -0300

    Inicia Header

--- app/public/wp-content/themes/mslaw/components/header/content.php
+++ app/public/wp-content/themes/mslaw/components/header/content.php
@@ -21,14 +21,26 @@ if (!defined('ABSPATH')) exit;
 
     ?>
     <header id="it9-mslaw-header">
+        <div class="container-fluid header-info bg-primary">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 text-center text-white">
+                    <span>Rua Itabuna, nº. 158, Vl. Floresta, Sto André/SP – CEP 09050-210</span>
+                </div>
+                <div class="col-lg-6 col-md-12 text-center text-white">
+                    Redes Sociais
+                </div>
+            </div>
+        </div>
+        <!--
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <div class="header-content">
-                        <div class="bg-primary"></div>
+                    <div class="header-content bg-primary" style="height: 50px;">
+                        
                     </div>
                 </div>
             </div>
         </div>
+        -->
     </header>
     <main>

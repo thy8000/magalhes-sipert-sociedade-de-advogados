f5be747a7e0df199ec03a4bef0d67a5f54f75467
Thunay Moreira de Soares <thunay@buildbox.com.br>
2022-09-26 12:55:47 -0300

    Continua Header

--- app/public/wp-content/themes/mslaw/components/header/content.php
+++ app/public/wp-content/themes/mslaw/components/header/content.php
@@ -21,13 +21,38 @@ if (!defined('ABSPATH')) exit;
 
     ?>
     <header id="it9-mslaw-header">
-        <div class="container-fluid header-info bg-primary">
+        <div class="header-info container-fluid bg-primary py-2">
             <div class="row">
-                <div class="col-lg-6 col-md-12 text-center text-white">
-                    <span>Rua Itabuna, nº. 158, Vl. Floresta, Sto André/SP – CEP 09050-210</span>
+                <div class="header-info__address col-lg-6 col-md-12 text-center text-white d-flex align-items-center">
+                    <span class="d-flex align-items-center">
+                        <img class="icon pe-2" src="<?php echo get_template_directory_uri() ?>/assets/images/icons/map-pin.svg">
+                        <a href="https://goo.gl/maps/wEUWgRZtNKrd7cvD9">
+                            Rua Itabuna, nº. 158, Vl. Floresta, Sto André/SP – CEP 09050-210
+                        </a>
+                    </span>
                 </div>
-                <div class="col-lg-6 col-md-12 text-center text-white">
-                    Redes Sociais
+                <div class="header-info__social col-lg-6 col-md-12 text-center text-white">
+                    <span>
+                        <img class="icon" src="<?php echo get_template_directory_uri() ?>/assets/images/icons/whatsapp.svg">
+                    </span>
+                    <span>
+                        <img class="icon" src="<?php echo get_template_directory_uri() ?>/assets/images/icons/youtube.svg">
+                    </span>
+                    <span>
+                        <img class="icon" src="<?php echo get_template_directory_uri() ?>/assets/images/icons/instagram.svg">
+                    </span>
+                    <span>
+                        <img class="icon" src="<?php echo get_template_directory_uri() ?>/assets/images/icons/facebook.svg">
+                    </span>
+                    <span>
+                        <img class="icon" src="<?php echo get_template_directory_uri() ?>/assets/images/icons/messenger.svg">
+                    </span>
+                    <span>
+                        <img class="icon" src="<?php echo get_template_directory_uri() ?>/assets/images/icons/linkedin.svg">
+                    </span>
+                    <span>
+                        <img class="icon" src="<?php echo get_template_directory_uri() ?>/assets/images/icons/twitter.svg">
+                    </span>
                 </div>
             </div>
         </div>

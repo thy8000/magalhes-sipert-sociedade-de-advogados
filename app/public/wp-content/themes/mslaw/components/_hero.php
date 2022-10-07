77309b64ce587e2b50401ad523e5c0a3941c6269
Thunay Moreira de Soares <thunay@buildbox.com.br>
2022-10-06 23:33:22 -0300

    Home Page Hero

---
+++ app/public/wp-content/themes/mslaw/components/_hero.php
@@ -0,0 +1,21 @@
+<div class="hero d-flex align-items-center justify-content-center text-center">
+    <div class="hero__text text-white px-3">
+        <?php
+        if (!empty($args['hero_title'])) {
+        ?>
+            <h1 class="hero__title h1 text-two"><?php echo esc_html($args['hero_title']); ?></h1>
+        <?php
+        }
+        if (!empty($args['hero_subtitle'])) {
+        ?>
+            <h2 class="hero__subtitle h2 text-light"><?php echo esc_html($args['hero_subtitle']); ?></h2>
+        <?php
+        }
+        if (!empty($args['hero_button_text'])) {
+        ?>
+            <button class="hero__button mt-5"><?php echo esc_html($args['hero_button_text']); ?></button>
+        <?php
+        }
+        ?>
+    </div>
+</div>
--- app/public/wp-content/themes/mslaw/pages/home/content.php
+++ app/public/wp-content/themes/mslaw/pages/home/content.php
@@ -1,7 +1,9 @@
-<div class="hero-image d-flex align-items-center justify-content-center text-center">
-  <div class="hero-text text-white px-3">
-    <h1 class="h1 text-two">Magalhães & Sipert Sociedade de Advogados</h1>
-    <h2 class="h2 text-light">Escritório de Advocacia em Santo André - SP</h2>
-    <button class="mt-5">Entrar em contato</button>
-  </div>
+<div class="home">
+  <?php 
+    get_template_part('components/_hero', null, [
+      'hero_title' => __('Magalhães & Sipert Sociedade de Advogados', 'mslaw'),
+      'hero_subtitle' => __('Escritório de Advocacia em Santo André - SP', 'mslaw'),
+      'hero_button_text' => __('Entrar em contato', 'mslaw'),
+    ]); 
+  ?>
 </div>

77570a47d95eb13f0b9ee694af6d530f817a1772
Thunay Moreira de Soares <thunay@buildbox.com.br>
2022-10-07 23:17:54 -0300

    Cria Flexible Card

---
+++ app/public/wp-content/themes/mslaw/components/_flexible-card.php
@@ -0,0 +1,55 @@
+<div class="gallery flexible__gallery">
+    <div class="container px-4 py-5" id="custom-cards">
+        <h2 class="title pb-2 text-one display-5 text-center fw-bold border-bottom">Serviços</h2>
+
+        <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+            <div class="col-lg-6">
+                <div class="card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-1.jpg');">
+                    <div class="d-flex flex-column text-white text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
+                        <h2 class="gallery__title fw-bold">Contencioso Civil</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-2.jpg');">
+                    <div class="d-flex flex-column text-white text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
+                        <h2 class="gallery__title fw-bold">Previdência e Trabalhista</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-3.jpg');">
+                    <div class="d-flex flex-column text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
+                        <h2 class="gallery__title fw-bold">Contratos</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-1.jpg');">
+                    <div class="d-flex flex-column text-white text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
+                        <h2 class="gallery__title fw-bold">Contencioso Civil</h2>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-lg-3">
+                <div class="card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-2.jpg');">
+                    <div class="d-flex flex-column text-white text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
+                        <h2 class="gallery__title fw-bold">Previdência e Trabalhista</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-3.jpg');">
+                    <div class="d-flex flex-column text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
+                        <h2 class="gallery__title fw-bold">Contratos</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- app/public/wp-content/themes/mslaw/pages/home/content.php
+++ app/public/wp-content/themes/mslaw/pages/home/content.php
@@ -1,15 +1,23 @@
 <div class="home">
-  <?php 
-    get_template_part('components/_hero', null, [
-      'hero_title' => __('Magalhães & Sipert Sociedade de Advogados', 'mslaw'),
-      'hero_subtitle' => __('Escritório de Advocacia em Santo André - SP', 'mslaw'),
-      'hero_button_text' => __('Entrar em contato', 'mslaw'),
-    ]); 
-
+  <?php
+  get_template_part('components/_hero', null, [
+    'hero_title' => __('Magalhães & Sipert Sociedade de Advogados', 'mslaw'),
+    'hero_subtitle' => __('Escritório de Advocacia em Santo André - SP', 'mslaw'),
+    'hero_button_text' => __('Entrar em contato', 'mslaw'),
+  ]);
+  ?>
+  <div class="bg-white">
+    <?php
     get_template_part('components/_vertical-hero', null, [
       'hero_title' => __('Quem Somos', 'mslaw'),
       'hero_text' => __('Magalhães & Sipert Sociedade de Advogados nasceu da vontade de seus sócios em atender seus clientes com uma prestação de serviços que agregue valor de forma a melhorarem o resultado final, tornando-se um bom parceiro.', 'mslaw'),
-      'hero_button_text' => __('Saiba mais', 'mslaw'),      
+      'hero_button_text' => __('Saiba mais', 'mslaw'),
     ]);
-  ?>
+    ?>
+  </div>
+  <div class="bg-light">
+    <?php
+    get_template_part('components/_flexible-card');
+    ?>
+  </div>
 </div>

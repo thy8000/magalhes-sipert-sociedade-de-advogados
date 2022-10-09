cc93bb7bf52ca3741b80afb337a70429f741a55d
Thunay Moreira de Soares <thunay@buildbox.com.br>
2022-10-09 16:25:36 -0300

    Cria Centered Hero

---
+++ app/public/wp-content/themes/mslaw/components/_centered-hero.php
@@ -0,0 +1,35 @@
+<?php
+$background_opacity = 0.5;
+?>
+<div class="hero centered__hero" style="background-image: linear-gradient(rgba(0, 0, 0, <?php echo $background_opacity; ?>), rgba(0, 0, 0, <?php echo $background_opacity; ?>)), url(<?php echo get_template_directory_uri(); ?>/assets/images/background/centered-hero.jpg);">
+    <div class="px-4 py-5 my-5 text-center text-white">
+        <?php
+        if (!empty($args['hero_title'])) 
+        {
+        ?>
+            <h1 class="display-5 fw-bold text-one"><?php echo esc_html($args['hero_title']); ?></h1>
+        <?php
+        }
+        ?>
+        <div class="col-lg-6 mx-auto">
+            <?php
+            if (!empty($args['hero_text'])) 
+            {
+            ?>
+                <p class="lead mb-4"><?php echo esc_html($args['hero_text']); ?></p>
+            <?php
+            }
+            ?>
+            <?php
+            if (!empty($args['hero_button_text'])) 
+            {
+            ?>
+                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <button class="hero__button mt-5"><?php echo esc_html($args['hero_button_text']); ?></button>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+    </div>
+</div>
--- app/public/wp-content/themes/mslaw/pages/home/content.php
+++ app/public/wp-content/themes/mslaw/pages/home/content.php
@@ -25,4 +25,11 @@
       get_template_part('components/_posts-cards');
     ?>
   </div>
+  <div class="bg-white">
+    <?php get_template_part('components/_centered-hero', null, [
+      'hero_title' => __('Fale Conosco', 'mslaw'),
+      'hero_text'  => __('Nosso escritório conta com advogados especializados em diversas áreas do Direito para resolver seus problemas', 'mslaw'),
+      'hero_button_text' => __('Entre em contato', 'mslaw')
+    ]); ?>
+  </div>
 </div>

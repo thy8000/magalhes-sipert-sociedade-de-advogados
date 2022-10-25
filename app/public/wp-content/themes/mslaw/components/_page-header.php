e02ecc37349b58275b3289f233324513a44e7711
Thunay Moreira de Soares <thunay@buildbox.com.br>
2022-10-24 23:08:29 -0300

    Ajustes Componentes Page Header

--- app/public/wp-content/themes/mslaw/components/_page-header.php
+++ app/public/wp-content/themes/mslaw/components/_page-header.php
@@ -1,11 +1,20 @@
+<?php
+$page_description = empty($args['page_description']) ? false : $args['page_description'];
+?>
 <div class="page-header d-flex justify-content-center align-items-center" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
     url(http://magalhes-sipert-sociedade-de-advogados.local/wp-content/uploads/2022/10/page-header-background.jpg);">
     <div class="page-header__box rounded">
         <div class="page-header__title">
             <h1 class="hero__title text-light display-2 lh-2 fw-bold text-center"><?php echo get_the_title(); ?></h1>
         </div>
-        <div class="page-header__subtitle">
-            <h2 class="hero__subtitle text-two fs-6 lh-base fw-normal text-center text-uppercase">Conheça o nosso trabalho</h2>
-        </div>
+        <?php
+        if (!empty($page_description)) {
+        ?>
+            <div class="page-header__subtitle">
+                <h2 class="hero__subtitle text-two fs-6 lh-base fw-normal text-center text-uppercase"><?php echo esc_html($page_description); ?></h2>
+            </div>
+        <?php
+        }
+        ?>
     </div>
 </div>

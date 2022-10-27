b70b56471a60ac0623fe0156b7d238b7eb60d98d
Thunay Moreira de Soares <thunay@buildbox.com.br>
2022-10-26 23:36:24 -0300

    Arruma Post Header

--- app/public/wp-content/themes/mslaw/components/_page-header.php
+++ app/public/wp-content/themes/mslaw/components/_page-header.php
@@ -1,11 +1,12 @@
 <?php
+$page_title = empty($args['page_title']) ? get_the_title() : $args['page_title'];
 $page_description = empty($args['page_description']) ? false : $args['page_description'];
 ?>
 <div class="page-header d-flex justify-content-center align-items-center" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
     url(http://magalhes-sipert-sociedade-de-advogados.local/wp-content/uploads/2022/10/page-header-background.jpg);">
     <div class="page-header__box rounded">
         <div class="page-header__title">
-            <h1 class="hero__title text-light display-2 lh-2 fw-bold text-center"><?php echo get_the_title(); ?></h1>
+            <h1 class="hero__title text-light display-2 lh-2 fw-bold text-center"><?php echo esc_html($page_title); ?></h1>
         </div>
         <?php
         if (!empty($page_description)) {

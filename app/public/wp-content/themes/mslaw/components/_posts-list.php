6e1fea6034853b3f33a313d74f1ef5e232ba8e57
Thunay Moreira de Soares <thunay@buildbox.com.br>
2022-10-27 23:08:23 -0300

    Ajustes Componente Posts List

--- app/public/wp-content/themes/mslaw/components/_posts-list.php
+++ app/public/wp-content/themes/mslaw/components/_posts-list.php
@@ -9,7 +9,7 @@ if (empty($posts_args))
 <div class="posts-list">
     <?php
     $posts_list = get_posts($posts_args);
-    debug($posts_list);
+    //debug($posts_list);
 
     if (empty($posts_list))
         return;
@@ -17,19 +17,33 @@ if (empty($posts_args))
     <div class="row">
         <?php
         foreach ($posts_list as $post) {
+            $post_thumbnail = '';
+
+            if (has_post_thumbnail($post->ID))
+                $post_thumbnail = get_the_post_thumbnail_url($post->ID);
+
+            if (!empty($post_thumbnail))
+                $post_style_tag = "style=\"background-image: url('{$post_thumbnail}') !important;\"";
+
+            $post_date = '';
+            if (!empty(get_the_date('YMD', $post->ID)))
+                $post_date = date_i18n('d') . ' de ' . date_i18n('F') . ' de ' . date_i18n('Y');
         ?>
-            <div class="col-lg-6 col-md-12">
+            <div class="col-lg-6 col-md-12 py-3">
                 <div class="post__item">
-                    <?php
-                    if (has_post_thumbnail($post->ID)) {
-                    ?>
-                        <div class="post__thumbnail"></div>
-                    <?php
-                    }
-                    ?>
-                    <div class="post__info"></div>
-                    <div class="post__title"></div>
-                    <div class="post__description"></div>
+                    <div class="post__thumbnail" <?php echo $post_style_tag; ?>></div>
+                    <div class="post__info">
+                        <ul class="p-0">
+                            <li><?php echo $post_date; ?></li>
+                            <li><?php echo get_the_author($post->ID); ?></li>
+                        </ul>
+                    </div>
+                    <div class="post__title text-one fs-5 fw-bold lh-base pt-3">
+                        <?php echo get_the_title($post->ID); ?>
+                    </div>
+                    <div class="post__description text-secondary fs-6 fw-normal lh-base py-3">
+                        <?php echo get_the_excerpt($post->ID); ?>
+                    </div>
                     <div class="post_button"></div>
                 </div>
             </div>

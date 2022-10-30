dbd5c32d04e979fb6bdf2405e3f0e6d076b6c9d0
Thunay <thunaymoreiradesoares@gmail.com>
2022-10-30 16:23:59 -0300

    Ajustes Componente Post List

--- app/public/wp-content/themes/mslaw/components/_posts-list.php
+++ app/public/wp-content/themes/mslaw/components/_posts-list.php
@@ -9,7 +9,6 @@ if (empty($posts_args))
 <div class="posts-list">
     <?php
     $posts_list = get_posts($posts_args);
-    //debug($posts_list);
 
     if (empty($posts_list))
         return;
@@ -18,6 +17,10 @@ if (empty($posts_args))
         <?php
         foreach ($posts_list as $post) {
             $post_thumbnail = '';
+            $post_category = get_the_category();
+
+            if (!empty($post_category))
+                $post_category = $post_category[0]->name;
 
             if (has_post_thumbnail($post->ID))
                 $post_thumbnail = get_the_post_thumbnail_url($post->ID);
@@ -31,20 +34,38 @@ if (empty($posts_args))
         ?>
             <div class="col-lg-6 col-md-12 py-3">
                 <div class="post__item">
-                    <div class="post__thumbnail" <?php echo $post_style_tag; ?>></div>
+                    <div class="post__thumbnail position-relative" <?php echo $post_style_tag; ?>>
+                        <?php if (!empty($post_category)) { ?>
+                            <div class="post__category bg-one text-light p-3 position-absolute left-0 fw-normal lh-base">
+                                <?php echo $post_category; ?>
+                            </div>
+                        <?php } ?>
+                    </div>
                     <div class="post__info">
-                        <ul class="p-0">
-                            <li><?php echo $post_date; ?></li>
-                            <li><?php echo get_the_author($post->ID); ?></li>
+                        <ul class="p-0 mt-1 d-flex">
+                            <li class="d-flex align-items-center justify-content-start">
+                                <?php echo it9_mslaws_get_svg_icon('clock.svg', 'icon'); ?>
+                                <span class="text fw-normal lh-base text-secondary">
+                                    <?php echo $post_date; ?>
+                                </span>
+                            </li>
+                            <li class="d-flex align-items-center justify-content-start ps-3">
+                                <?php echo it9_mslaws_get_svg_icon('user.svg', 'icon'); ?>
+                                <span class="text fw-normal lh-base text-secondary">
+                                    <?php echo get_the_author_meta('display_name', $post->post_author); ?>
+                                </span>
+                            </li>
                         </ul>
                     </div>
-                    <div class="post__title text-one fs-5 fw-bold lh-base pt-3">
+                    <div class="post__title text-one fs-5 fw-bold lh-base pt-1">
                         <?php echo get_the_title($post->ID); ?>
                     </div>
-                    <div class="post__description text-secondary fs-6 fw-normal lh-base py-3">
-                        <?php echo get_the_excerpt($post->ID); ?>
+                    <div class="post__read-more d-flex align-items-center">
+                        <a class="text d-flex align-items-center fw-bold lh-base text-secondary text-uppercase" href="<?php echo get_the_permalink($post->ID); ?>">
+                            Leia mais
+                        </a>
+                        <?php echo it9_mslaws_get_svg_icon('arrow-right.svg', 'icon d-flex align-items-center'); ?>
                     </div>
-                    <div class="post_button"></div>
                 </div>
             </div>
         <?php

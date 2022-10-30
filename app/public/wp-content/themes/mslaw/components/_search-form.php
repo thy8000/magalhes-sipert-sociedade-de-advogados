bcafeeedcc2331eb3fbeded11830c78880c54626
Thunay <thunaymoreiradesoares@gmail.com>
2022-10-30 17:32:57 -0300

    Arruma Componente Search Form

---
+++ app/public/wp-content/themes/mslaw/components/_search-form.php
@@ -0,0 +1,10 @@
+<div class="search-form">
+    <h5 class="text-uppercase mb-4 pb-1 fw-normal lh-base fs-5">Pesquisar</h5>
+
+    <div class="search input-group mb-3 d-flex">
+        <input type="text" class="search__input form-control position-absolute" placeholder="Pesquisar" aria-label="Pesquisar" aria-describedby="button-addon2">
+        <button class="search__button btn absolute" type="button" id="button-addon2">
+            <?php echo it9_mslaws_get_svg_icon('search.svg', 'search__icon') ?>
+        </button>
+    </div>
+</div>

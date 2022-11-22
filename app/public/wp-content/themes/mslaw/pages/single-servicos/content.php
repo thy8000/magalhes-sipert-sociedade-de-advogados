d20e540863173f26e5f75d57d683a359a6badbe9
Thunay <thunaymoreiradesoares@gmail.com>
2022-11-21 22:43:10 -0300

    Ajustes CSS Componentes Single Services

--- app/public/wp-content/themes/mslaw/pages/single-servicos/content.php
+++ app/public/wp-content/themes/mslaw/pages/single-servicos/content.php
@@ -5,9 +5,11 @@ $post_id = get_the_ID();
 
 ?>
 <div class="single-services">
-<?php
+    <?php
     get_template_part('pages/single/components/post-thumbnail', null, [
         'post_id' => $post_id,
     ]);
-?>
+
+    get_template_part('pages/single-servicos/components/services-double-column'); 
+    ?>
 </div>

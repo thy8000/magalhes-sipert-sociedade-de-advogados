c397878dd8fe923a3bf53db16120f22093ffcdf8
Thunay <thunaymoreiradesoares@gmail.com>
2022-11-24 22:43:01 -0300

    Adiciona ACF e TGMPA

--- app/public/wp-content/themes/mslaw/functions.php
+++ app/public/wp-content/themes/mslaw/functions.php
@@ -3,5 +3,6 @@ if (!defined('ABSPATH')) exit;
 
 require_once implode(DIRECTORY_SEPARATOR, [get_template_directory(), 'includes', 'global', '_index.php']);
 require_once implode(DIRECTORY_SEPARATOR, [get_template_directory(), 'includes', 'services', '_index.php']);
+require_once implode(DIRECTORY_SEPARATOR, [get_template_directory(), 'includes', 'tgmpa', '_index.php']);
 require_once implode(DIRECTORY_SEPARATOR, [get_template_directory(), 'includes', 'utils', '_index.php']);
 require_once implode(DIRECTORY_SEPARATOR, [get_template_directory(), 'includes', 'wp-bootstrap-walker', '_index.php']);

a86b9fd750e3547254ed5be74e43ee2379df7f76
Thunay <thunaymoreiradesoares@gmail.com>
2022-12-11 15:53:12 -0300

    Ajustes Post Type Serviços

--- app/public/wp-content/themes/mslaw/components/_gallery.php
+++ app/public/wp-content/themes/mslaw/components/_gallery.php
@@ -2,6 +2,15 @@
 $background_opacity = 0.6;
 $title = empty($args['title']) ? false : $args['title'];
 $description = empty($args['description']) ? false : $args['description'];
+
+$services_posts = get_posts([
+    'post_type' => 'servicos',
+    'posts_per_page' => 9,
+    'orderby' => 'date',
+    'order' => 'DESC',
+]);
+
+if (empty($services_posts)) return;
 ?>
 <div class="gallery flexible__gallery py-5">
     <div class="container px-4 py-5" id="custom-cards">
@@ -18,53 +27,43 @@ $description = empty($args['description']) ? false : $args['description'];
         }
             ?>
             <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-                <div class="col-lg-6 gallery__card">
-                    <div class="gallery__image card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: linear-gradient(rgba(0, 0, 0, <?php echo $background_opacity; ?>), rgba(0, 0, 0, <?php echo $background_opacity; ?>)), url(<?php echo get_template_directory_uri(); ?>/assets/images/background/contencioso-civil.jpg);">
-                        <div class="d-flex flex-column text-white text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
-                            <h2 class="gallery__title fw-bold">Contencioso Civil</h2>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                $post_count = 0;
+                foreach ($services_posts as $post) {
+                    $post_count++;
 
-                <div class="col-lg-3 gallery__card">
-                    <div class="gallery__image card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: linear-gradient(rgba(0, 0, 0, <?php echo $background_opacity; ?>), rgba(0, 0, 0, <?php echo $background_opacity; ?>)), url(<?php echo get_template_directory_uri(); ?>/assets/images/background/previdencia-trabalhista.jpg);">
-                        <div class="d-flex flex-column text-white text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
-                            <h2 class="gallery__title fw-bold">Previdência e Trabalhista</h2>
+                    if ($post_count == 1 || $post_count == 6) {
+                        $post_count = $post_count == 6 ? $post_count = 0 : $post_count;
+                ?>
+                        <div class="col-lg-6 gallery__card">
+                            <a href="<?php echo esc_url(get_the_permalink($post->ID)); ?>">
+                                <div class="gallery__image card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: linear-gradient(rgba(0, 0, 0, <?php echo $background_opacity; ?>), rgba(0, 0, 0, <?php echo $background_opacity; ?>)), url(<?php echo esc_url(get_the_post_thumbnail_url($post->ID)); ?>)">
+                                    <div class="d-flex flex-column text-white text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
+                                        <h2 class="gallery__title fw-bold">
+                                            <?php esc_html_e($post->post_title, 'it9_mslaw'); ?>
+                                        </h2>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
+                    <?php
+                        continue;
+                    }
+                    ?>
+                    <div class="col-lg-3 gallery__card">
+                        <a href="<?php echo esc_url(get_the_permalink($post->ID)); ?>">
+                            <div class="gallery__image card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: linear-gradient(rgba(0, 0, 0, <?php echo $background_opacity; ?>), rgba(0, 0, 0, <?php echo $background_opacity; ?>)), url(<?php echo esc_url(get_the_post_thumbnail_url($post->ID)); ?>)">
+                                <div class="d-flex flex-column text-white text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
+                                    <h2 class="gallery__title fw-bold">
+                                        <?php esc_html_e($post->post_title, 'it9_mslaw'); ?>
+                                    </h2>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                </div>
-
-                <div class="col-lg-3 gallery__card">
-                    <div class="gallery__image card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: linear-gradient(rgba(0, 0, 0, <?php echo $background_opacity; ?>), rgba(0, 0, 0, <?php echo $background_opacity; ?>)), url(<?php echo get_template_directory_uri(); ?>/assets/images/background/tributario.jpg);">
-                        <div class="d-flex flex-column text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
-                            <h2 class="gallery__title fw-bold">Tributário</h2>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 gallery__card">
-                    <div class="gallery__image card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: linear-gradient(rgba(0, 0, 0, <?php echo $background_opacity; ?>), rgba(0, 0, 0, <?php echo $background_opacity; ?>)), url(<?php echo get_template_directory_uri(); ?>/assets/images/background/imobiliario.jpg);">
-                        <div class="d-flex flex-column text-white text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
-                            <h2 class="gallery__title fw-bold">Imobiliário </h2>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 gallery__card">
-                    <div class="gallery__image card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: linear-gradient(rgba(0, 0, 0, <?php echo $background_opacity; ?>), rgba(0, 0, 0, <?php echo $background_opacity; ?>)), url(<?php echo get_template_directory_uri(); ?>/assets/images/background/societario.jpg);">
-                        <div class="d-flex flex-column text-white text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
-                            <h2 class="gallery__title fw-bold">Societário </h2>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-6 gallery__card">
-                    <div class="gallery__image card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(<?php echo get_template_directory_uri(); ?>/assets/images/background/consumidor.jpg);">
-                        <div class="d-flex flex-column text-shadow-1 align-items-center justify-content-center text-center h-100 p-3">
-                            <h2 class="gallery__title fw-bold">Consumidor</h2>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                }
+                ?>
             </div>
     </div>
 </div>

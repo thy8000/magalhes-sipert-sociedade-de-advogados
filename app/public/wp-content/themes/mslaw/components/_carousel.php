b32e333bbc6629367a0a46027e15fe99e04bae37
Thunay <thunaymoreiradesoares@gmail.com>
2023-01-06 17:15:30 -0300

    Cria Componente Carousel

---
+++ app/public/wp-content/themes/mslaw/components/_carousel.php
@@ -0,0 +1,40 @@
+<?php
+if (!defined('ABSPATH')) exit;
+?>
+<div class="carousel">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center text-one fs-1 fw-bold lh-base">Conheça o nosso escritório</h2>
+                <h3 class="text-center text-secondary fs-5 fw-normal lh-base px-md-5 mx-md-5 px-0 mx-0">Confira imagens exclusivas do nosso escritório. Temos uma infraestrutura ideal para atender nossos clientes!</h3>
+            </div>
+        </div>
+    </div>
+    <div class="carousel-container pt-3">
+        <div class="carousel-slide fade">
+            <div class="numbertext">1 / 3</div>
+            <div class="carousel-image" style="background-image: url(http://magalhes-sipert-sociedade-de-advogados.local/wp-content/uploads/2022/10/mslaws-vertical-hero-image.jpg);"></div>
+            <div class="text">Caption Text</div>
+        </div>
+
+        <div class="carousel-slide fade">
+            <div class="numbertext">2 / 3</div>
+            <div class="carousel-image" style="background-image: url(http://magalhes-sipert-sociedade-de-advogados.local/wp-content/uploads/2022/10/hero-background-main-page.jpg);"></div>
+            <div class="text">Caption Two</div>
+        </div>
+
+        <div class="carousel-slide fade">
+            <div class="numbertext">3 / 3</div>
+            <div class="carousel-image" style="background-image: url(http://magalhes-sipert-sociedade-de-advogados.local/wp-content/uploads/2022/10/magalhaes-hero.jpg);"></div>
+            <div class="text">Caption Three</div>
+        </div>
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    </div>
+    <br>
+    <div class="pt-3" style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+</div>
--- app/public/wp-content/themes/mslaw/pages/page-escritorio/content.php
+++ app/public/wp-content/themes/mslaw/pages/page-escritorio/content.php
@@ -19,11 +19,17 @@ if (!defined('ABSPATH')) exit;
     </div>
     <div class="bg-white pt-5">
         <?php
-            get_template_part('components/_google-maps-section', null, [
-                'title' => esc_html__('Localização', 'it9_mslaws'),
-                'description' => esc_html__('Saiba onde o nosso escritório está localizado.', 'it9_mslaws'),
-                'iframe_embed_link' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.143830442472!2d-46.54456932479151!3d-23.670813878724754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce4258e2eeffa5%3A0x5b872d466d17dfc9!2sR.%20Itabuna%2C%20158%20-%20Vila%20Floresta%2C%20Santo%20Andr%C3%A9%20-%20SP%2C%2009050-230!5e0!3m2!1spt-BR!2sbr!4v1672968333344!5m2!1spt-BR!2sbr',
-            ]);
+        get_template_part('components/_google-maps-section', null, [
+            'title' => esc_html__('Localização', 'it9_mslaws'),
+            'description' => esc_html__('Saiba onde o nosso escritório está localizado.', 'it9_mslaws'),
+            'iframe_embed_link' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.143830442472!2d-46.54456932479151!3d-23.670813878724754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce4258e2eeffa5%3A0x5b872d466d17dfc9!2sR.%20Itabuna%2C%20158%20-%20Vila%20Floresta%2C%20Santo%20Andr%C3%A9%20-%20SP%2C%2009050-230!5e0!3m2!1spt-BR!2sbr!4v1672968333344!5m2!1spt-BR!2sbr',
+        ]);
         ?>
+    </div>
+    <div class="bg-light pt-5">
+        <?php get_template_part('components/_carousel'); ?>
+    </div>
+    <div class="bg-light mt-5">
+        <?php get_template_part('components/_centered-hero'); ?>
     </div>
 </div>

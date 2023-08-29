153be8f04298d93cc748f756612088d79b6270dc
Thunay <thunaymoreiradesoares@gmail.com>
2023-08-28 23:30:46 -0300

    Adiciona escritorio fields

--- app/public/wp-content/themes/mslaw/includes/global/_index.php
+++ app/public/wp-content/themes/mslaw/includes/global/_index.php
@@ -10,3 +10,4 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'add-logo-support.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'add-post-thumbnail-support.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'disable-wp-blocks.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'override-main-query.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'add-escritorio-fields.php';
---
+++ app/public/wp-content/themes/mslaw/includes/global/add-escritorio-fields.php
@@ -0,0 +1,101 @@
+<?php
+
+if (function_exists('acf_add_local_field_group')) :
+
+    acf_add_local_field_group(array(
+        'key' => 'group_64ed5309574e5',
+        'title' => 'Escritório - Slides',
+        'fields' => array(
+            array(
+                'key' => 'field_64ed5309e4766',
+                'label' => 'Lista de Slides',
+                'name' => 'escritorio_slides_list',
+                'aria-label' => '',
+                'type' => 'repeater',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'layout' => 'table',
+                'pagination' => 0,
+                'min' => 0,
+                'max' => 0,
+                'collapsed' => '',
+                'button_label' => 'Adicionar Linha',
+                'rows_per_page' => 20,
+                'sub_fields' => array(
+                    array(
+                        'key' => 'field_64ed53a8e4767',
+                        'label' => 'image',
+                        'name' => 'image',
+                        'aria-label' => '',
+                        'type' => 'image',
+                        'instructions' => '',
+                        'required' => 1,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'return_format' => 'url',
+                        'library' => 'all',
+                        'min_width' => '',
+                        'min_height' => '',
+                        'min_size' => '',
+                        'max_width' => '',
+                        'max_height' => '',
+                        'max_size' => '',
+                        'mime_types' => '',
+                        'preview_size' => 'medium',
+                        'parent_repeater' => 'field_64ed5309e4766',
+                    ),
+                    array(
+                        'key' => 'field_64ed53dee4768',
+                        'label' => 'Legenda',
+                        'name' => 'legend',
+                        'aria-label' => '',
+                        'type' => 'text',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '',
+                        'maxlength' => '',
+                        'placeholder' => '',
+                        'prepend' => '',
+                        'append' => '',
+                        'parent_repeater' => 'field_64ed5309e4766',
+                    ),
+                ),
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'page',
+                    'operator' => '==',
+                    'value' => '174',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+        'show_in_rest' => 0,
+    ));
+
+endif;
